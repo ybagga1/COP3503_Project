5741a56ce83c38e5cd7000a5ab9ee4608d8e02a7
ndiez <ndiez@ufl.edu>
2014-03-31 09:04:07 -0400

    Created Operations.cpp Skeleton

---
+++ Operations.cpp
@@ -0,0 +1,32 @@
+#include "Operations.h"
+
+Operations::Operations() {
+
+}
+Operations::~Operations() {
+
+}
+Number Operations::add(Number a, Number b) {
+
+}
+Number Operations::subtract(Number a, Number b) {
+
+}
+Number Operations::multiply(Number a, Number b) {
+
+}
+Number Operations::divide(Number a, Number b) {
+
+}
+Number Operations::exponentiate(Number a, Number b) {
+
+}
+Number Operations::logbase(Number a, Number b) {
+
+}
+Number Operations::nthroot(Number a, Number b) {
+
+}
+Number Operations::simplifyAns(Number a, Number b) {
+
+}
