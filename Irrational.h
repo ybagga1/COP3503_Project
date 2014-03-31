3461a6cbd4ac3e6bd3af02934206d4339ac6500d
ndiez <ndiez@ufl.edu>
2014-03-31 08:57:07 -0400

    Created Irrational.h

---
+++ Irrational.h
@@ -0,0 +1,15 @@
+#ifndef IRRATIONAL_H_
+#define IRRATIONAL_H_
+#include <string>
+using namespace std;
+
+class Irrational : public Number {
+public:
+	// default constructor
+	Irrational();
+	// addition/subtraction/division/multiplication/nthroot/log constructor
+	Irrational(Number a, string operand, Number b);
+	~Irrational();
+};
+
+#endif /* IRRATIONAL_H_ */
