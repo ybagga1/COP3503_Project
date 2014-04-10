21c174b09288d5de92413287d37a2bb5ae9d5b63
dvorakk <dvorakk826@gmail.com>
2014-04-10 09:49:35 -0400

    Update Rational.cpp

--- Rational.cpp
+++ Rational.cpp
@@ -1,8 +1,43 @@
-#include "Rational.h"
-Rational::Rational() {
+#include 'Rational.h'
 
+
+Rational:: Rational(){
+	numerator = 1;
+	denominator = 1;
+	type = "Rational";
+	value = 1;
 }
-Number Rational::simplify(int a, int b) {
+
+Rational:: Rational(int numerator){
+	this.numerator = numerator;
+	this.denominator = 1;
+	type = "Rational";
+	value = numerator;
+}
+
+Rational:: Rational(int numerator, int denominator){
+	this.numerator = numerator;
+	this.denominator = denominator;
+	simplify();
+	type = "Rational";
+	value = (float)numerator / (float)denominator;
+	
+}
+
+int Rational:: getNum(){
+	return numerator;
+}
+
+int Rational:: getDenom(){
+	return denominator;
+}
+
+string Rational:: toString(){
+	return numerator + ' '
+	
+}
+
+Rational Rational::simplify(int a, int b) {
 	if (a < b) {
 		for (int i = 2, i < b, i++) {
 			if (a % i == 0 && b % i == 0) {
