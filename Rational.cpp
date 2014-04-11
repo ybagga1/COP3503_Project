5f969da02843ffd5d5445399ce4a879053b37f70
dvorakk <dvorakk826@gmail.com>
2014-04-10 20:08:59 -0400

    Update Rational.cpp

--- Rational.cpp
+++ Rational.cpp
@@ -1,75 +1,79 @@
 #include 'Rational.h'
 
+
 Rational:: Rational(){
-numerator = 1;
-denominator = 1;
-type = "Rational";
-value = 1;
+	numerator = 1;
+	denominator = 1;
+	type = "Rational";
+	value = 1;
 }
 
 Rational:: Rational(int numerator){
-this->numerator = numerator;
-this->denominator = 1;
-type = "Rational";
-value = numerator;
+	this->numerator = numerator;
+	this->denominator = 1;
+	type = "Rational";
+	value = numerator;
 }
 
 Rational:: Rational(int numerator, int denominator){
-if(denominator == 0)
-throw invalid_argument("Cannot divide by zero");
-
-this->numerator = numerator;
-this->denominator = denominator;
-
-type = "Rational";
-value = (float)numerator / (float)denominator;
+	if(denominator == 0)
+		throw invalid_argument("Cannot divide by zero");
+		
+	this->numerator = numerator;
+	this->denominator = denominator;
+	
+	type = "Rational";
+	value = (float)numerator / (float)denominator;
+	
 }
 
+
 int Rational:: getNum(){
-return numerator;
+	return numerator;
 }
 
 int Rational:: getDenom(){
-return denominator;
+	return denominator;
 }
+
 
 //if denominator = 1, return the numerator as a string
 //else return "numerator '/' denominator"
 string Rational:: toString(){
-string number = "";
-return number;
-
+	string number = "";
+	return number;
+	
 }
-string Rational:: type()
+string Rational::  type()
 {
-return type;
+	return type;
 }
 
-float Rational:: value()
+float Rational::  value()
 {
-return value;
+	return value;
 }
 
 Rational Rational::simplify(Rational& r) {
-
-int a= r.getNum();
-int b = r.getDenom();
-if (a < b) {
-    for (int i = 2, i < b, i++) {
-        if (a % i == 0 && b % i == 0) {
-            a /= i;
-            b /= i;
-        }
-    }
-}
-else {
-    for (int i = 2, i < a, i++) {
-        if (b % i == 0 && a % i == 0) {
-            a /= i;
-            b /= i;
-        }
-    }
-}
-Number fraction = new Number(a, b);
-return fraction;
+	
+	int a= r.getNum();
+	int b = r.getDenom();
+	if (a < b) {
+		for (int i = 2, i < b, i++) {
+			if (a % i == 0 && b % i == 0) {
+				a /= i;
+				b /= i;
+			}
+		}
+	}
+	else {
+		for (int i = 2, i < a, i++) {
+			if (b % i == 0 && a % i == 0) {
+				a /= i;
+				b /= i;
+			}
+		}
+	}
+	Number fraction = new Number(a, b);
+	return fraction;
 }
