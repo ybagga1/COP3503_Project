bacd8d28071a32b9403c7380a63242ee47069a4b
ndiez <ndiez@ufl.edu>
2014-04-12 17:06:35 -0400

    Update Irrational.h

--- Irrational.h
+++ Irrational.h
@@ -1,6 +1,7 @@
 #ifndef IRRATIONAL_H_
 #define IRRATIONAL_H_
 #include <string>
+#include "Number.h"
 using namespace std;
 
 class Irrational : public Number {
@@ -8,11 +9,18 @@ public:
 	// default constructor
 	Irrational();
 	// addition/subtraction/division/multiplication/nthroot/log constructor
-	Irrational(Number a, string operand, Number b);
+	Irrational(string irr);
 	~Irrational();
+	Number simplify();
+	string toString();
+	string getType();
+	float getValue();
 private:
-	string type;
-	float value;
+	string base;
+	string logOf;
+	string sqrtOf;
+	string rootOf;
+	string n;
 };
 
 #endif /* IRRATIONAL_H_ */
