3a6538961b8b0076c3f7a2ab6d30e12ac2f64895
ndiez <ndiez@ufl.edu>
2014-04-12 21:12:12 -0400

    Update Number.h

--- Number.h
+++ Number.h
@@ -9,10 +9,10 @@ class Number {
 public:
 	virtual Number* simplify() = 0;
 	virtual string toString() = 0;
-	virtual string getType();
-	virtual float getValue();
-	virtual int getNum();
-	virtual int getDenom();
+	virtual string getType() = 0;
+	virtual float getValue() = 0;
+	virtual int getNum() = 0;
+	virtual int getDenom() = 0;
 
 protected:
 	string sType;
