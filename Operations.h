93a22e86ad55723b2bbb6370d06b4b5a873ff829
MatG123 <m.giaramita@gmail.com>
2014-04-13 01:05:34 -0400

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -1,6 +1,9 @@
 #ifndef OPERATIONS_H_
 #define OPERATIONS_H_
 #include "Number.h"
+#include <stdlib.h>
+#include <iostream>
+#include <string>
 class Operations {
 public:
 	Operations();
@@ -16,6 +19,9 @@ public:
 	Number* toRational(double a);
 private:
 	int aNum, aDenom, bNum, bDenom, ansNum, ansDenom;
+	float irrVal, irrVal2, ansVal;
+	string irrAns, irrStr, irrStr2;
 };
 
 #endif /* OPERATIONS_H_ */
+
