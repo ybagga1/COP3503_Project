5e97bc4634f9433682ca36f4f45768b8c822c58f
ybagga1 <ybagga1@ufl.edu>
2014-04-14 00:55:46 -0400

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <string>
 class Operations {
+    // This class does all the operations for number types. 
 public:
+    // Public operations include add, subtract, multiply, divide, and exponentiate. 
 	Operations();
 	~Operations();
 	Number* add(Number* a, Number* b);
@@ -15,9 +17,8 @@ public:
 	Number* exponentiate(Number* a, Number* b);
 	Number* toRational(string a);
 private:
+    // Private requirements for setting numerator & denominators, irrational values, logs, etc. 
 	int aNum, aDenom, bNum, bDenom, ansNum, ansDenom;
 	float irrVal, irrVal2, ansVal, logofa, logofb, logofans;
 	string irrAns, irrStr, irrStr2;
 };
-
-#endif /* OPERATIONS_H_ */
